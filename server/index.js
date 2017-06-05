69325fa344c25cac25455a584d0f9fb22cd6abf2
Tri Nguyen <tri@tridnguyen.com>
2017-06-05 00:03:41 -0400

    save sprites locally, and serve them

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const PouchDB = require('pouchdb');
 const fetch = require('node-fetch');
+const fse = require('fs-extra');
 const app = express();
 const SERVER_PORT = process.env.PORT || 3000;
 const POKEAPI_URL = 'http://pokeapi.co/api/v2/';
@@ -10,11 +11,28 @@ app.use(cors({
 	origin: 'http://localhost:9000'
 }));
 
-app.use('/db', require('express-pouchdb')(PouchDB));
-
 const db = new PouchDB('http://localhost:5984/pokemons');
 
-app.get('/pokemon/:id', (async (req, res) => {
+async function saveSprites (pokemon) {
+	if (!pokemon.sprites) {
+		console.log('no sprites');
+		return;
+	}
+	await Object.keys(pokemon.sprites).forEach(async (type) => {
+		if (!pokemon.sprites[type]) {
+			return;
+		}
+		let exists = await fse.pathExists(`./db/sprites/${pokemon.id}/${type}.png`);
+		if (exists) {
+			return;
+		}
+		let response = await fetch(pokemon.sprites[type]);
+		await fse.mkdirs(`./db/sprites/${pokemon.id}`);
+		response.body.pipe(fse.createWriteStream(`./db/sprites/${pokemon.id}/${type}.png`));
+	});
+}
+
+app.get('/pokemon/:id', async (req, res) => {
 	let pokemon;
 	try {
 		pokemon = await db.get(req.params.id);
@@ -25,18 +43,34 @@ app.get('/pokemon/:id', (async (req, res) => {
 			res.send(400).json(e);
 		}
 		let response = await fetch(POKEAPI_URL + 'pokemon/' + req.params.id);
-		let json = await response.json();
-		if (!json) {console.log(req.params.id)}
+		pokemon = await response.json();
+		if (!pokemon) {
+			return res.send(404).json({
+				error: `${req.params.id} is not found`
+			});
+		}
 		try {
-			await db.put(Object.assign({}, json, {
-				_id: json.id.toString()
+			await db.put(Object.assign({}, pokemon, {
+				// _id needs to be a string
+				_id: pokemon.id.toString()
 			}));
 		} catch (e) {
 			console.error(e);
 		}
-		res.json(json);
+		res.json(pokemon);
 	}
-}));
+	await saveSprites(pokemon);
+});
+
+app.get('/sprites/:pokemon/:type', async (req, res) => {
+	let filePath = `./db/sprites/${req.params.pokemon}/${req.params.type}`;
+	let exists = await fse.pathExists(filePath);
+	if (!exists) {
+		res.send(404);
+		return;
+	}
+	req.sendFile(filePath);
+});
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Express is listening on ${SERVER_PORT}`);
