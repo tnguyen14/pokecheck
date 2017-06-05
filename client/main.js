0cad73971944e3149e7d0ea4c58d2c21b9ac5bd7
Tri Nguyen <tri@tridnguyen.com>
2017-06-04 23:00:17 -0400

    render pokemons

--- client/main.js
+++ client/main.js
@@ -3,10 +3,29 @@
 const SERVER_URL = 'http://localhost:3000';
 const NUM_POKEMONS = 10;
 
-Array.from(new Array(NUM_POKEMONS), (val,index) => index + 1).map((index) => {
-	fetch(SERVER_URL + '/pokemon/' + index).then((res) => {
-		return res.json();
-	}).then((json) => {
-		console.log(json);
-	});
+const appEl = document.querySelector('#app');
+
+function createPokemonEl (pokemon) {
+	let el = document.createElement('div');
+	el.classList.add('pokemon');
+	let name = document.createElement('div');
+	name.innerHTML = pokemon.name;
+	el.appendChild(name);
+	return el;
+}
+
+async function getPokemons (numPokemons) {
+	// make array of number from 1 - NUM_POKEMONS
+	return await Promise.all(Array.from(new Array(numPokemons), (val, index) => index + 1).map(async (index) => {
+		let response = await fetch(SERVER_URL + '/pokemon/' + index);
+		let pokemon = await response.json();
+		return pokemon;
+	}));
+}
+
+// display each pokemon
+getPokemons(NUM_POKEMONS).then((pokemons) => {
+	pokemons.forEach((pokemon) => {
+		appEl.appendChild(createPokemonEl(pokemon));
+	})
 });
